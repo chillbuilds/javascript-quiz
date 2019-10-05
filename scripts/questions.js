946b04927c7cf9c8ce9cc8bfd07533cad310703b
heliumface770 <heliumface770@gmail.com>
2019-10-05 16:13:53 -0600

    questions.js placeholder

---
+++ scripts/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<javascript>", "<script>", "js"],
+      answer: "<script>"
+    },
+    {
+      title: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      choices: ["<script src='xxx.js'>", "<script href='xxx.js'>", "<script name='xxx.js'>"],
+      answer: "<script src='xxx.js'>"
+    },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function = myFunction()", "function myFunction()", "function:myFunction()"],
+        answer: "function myFunction()"
+      },
+  ];
