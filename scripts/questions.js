65bb6c27b8e5381e101ce7be980b173f62942a00
heliumface770 <heliumface770@gmail.com>
2019-10-05 21:34:38 -0600

    question cycle completed

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,15 +1,15 @@
 var questions = [
-    {
+    { number: "1",
       title: "Inside which HTML element do we put the JavaScript?",
       choices: ["<javascript>", "<script>", "js"],
       answer: "<script>"
     },
-    {
+    { number: "2",
       title: "What is the correct syntax for referring to an external script called 'xxx.js'?",
       choices: ["<script src='xxx.js'>", "<script href='xxx.js'>", "<script name='xxx.js'>"],
       answer: "<script src='xxx.js'>"
     },
-    {
+    {   number: "3",
         title: "How do you create a function in JavaScript?",
         choices: ["function = myFunction()", "function myFunction()", "function:myFunction()"],
         answer: "function myFunction()"
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,41 @@
 var startBtnEl = document.querySelector("#startBtn")
+var scoreBtnEl = document.querySelector("#scoreBtn")
+var questionNum = 0;
+var totalSeconds = 0;
+var secondsElapsed = 0;
+var questionsStr = JSON.stringify(questions);
+$("#timeHolder").text("Timer: " + 0);
+
+    // var question_number = [];
+    // for(i = 0; i < question.length; i++){
+    //     question_number.push(questions[i].number);
+    // }
+
 
 function quizStart(){
+    totalSeconds = questions.length * 15;     //arraylength * 5 seconds
+    $("#timeHolder").text("Timer: " + totalSeconds);
+    startBtn.innerHTML = ""; //placeholder
+    document.querySelector("#startBtn").id = 'questionContainer';
+    var newQuestion = questions[questionNum].title;
+    questionContainer.innerHTML = newQuestion;
+
+    // questionNum = questionNum + 1;
+    
 }
 
+function nextQuestion(){
+    questionNum = questionNum + 1;
+    var newQuestion = questions[questionNum].title;
+    questionContainer.innerHTML = newQuestion;
+}
+
+function highscores(){
+    nextQuestion();
+}
+
+
+
 startBtnEl.addEventListener("click", quizStart)
+
+scoreBtnEl.addEventListener("click", highscores)
